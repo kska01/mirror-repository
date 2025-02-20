efe13182d1526def50155ed15fc0287206a2ad70
bjh0499 <j9603h@gmail.com>
2025-02-20 14:18:55 +0900

    BCW-2 백엔드 환경 설정 (#1)
    * [BCW-2] chore: [BE] 환경 설정 및 .gitignore에 .env 추가
    
    * [BCW-2] chore: [BE] Spring에서 환경 변수 사용을 위한 Dependencies 추가

---
+++ back-end/src/main/java/com/ssb/scalendar/ScalendarApplication.java
@@ -0,0 +1,13 @@
+package com.ssb.scalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalendarApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/ssb/scalendar/ScalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssb.scalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
