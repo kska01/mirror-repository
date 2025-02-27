326295c8d8d496d3d4a93e6a22c032af4de08c85
KyungSik Ka <insideka01@gmail.com>
2025-02-27 14:17:35 +0900

    BCW-50 [FE] 헤더 리팩토링 및 로그인 유무에 따른 기능 추가 (#34)
    * [BCW-50] feat: 로그인에 따른 로그인/로그아웃 교체 기능 및 로그아웃 시 랜딩페이지로 이동 구현
    
    - header size, text-xl로 변경
    
    * [BCW-50] refactor: 숨김처리 로직을 더 간략하게 축소

--- fe/src/components/Header.jsx
+++ fe/src/components/Header.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { logout } from '../store/slices/authSlice';
+import { useDispatch } from 'react-redux';
 
 export default function Header() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const token = localStorage.getItem('token');
+  console.log(token);
+  
+  let hidden = !!token;
+
+  
+
+  const handleOnLogoutClick = () => {
+    dispatch(logout());
+    navigate('/');
+  };
+
   return (
     <>
-      <section className="flex justify-between">
+      <section className="flex justify-between text-xl">
         <div>S-Calendar</div>
         <nav className="flex gap-8">
           <Link to="/signup">회원가입</Link>
-          <Link to="/login">로그인</Link>
+          <Link to="/login" hidden={hidden}>
+            로그인
+          </Link>
+          <Link onClick={handleOnLogoutClick} hidden={!hidden}>
+            로그아웃
+          </Link>
         </nav>
       </section>
     </>
