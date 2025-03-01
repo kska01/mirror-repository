4b4b952567f59f90887f8db77c4d72945fc5f6f1
bjh0499 <j9603h@gmail.com>
2025-03-02 00:17:21 +0900

    BCW-55 [FE] 달력 세부 레이아웃 조정 (#43)
    * [BCW-55] chore: 달력 언어 변경을 한국어로 수정
    
    * [BCW-55] refactor: UI 수정의 편의를 위해 달력 component 내에 있던 버튼들을 이동
    
    * [BCW-55] test: 오늘 강조 색상 변경 기능 시범 추가
    
    * [BCW-55] feat: 일정 수에 따른 색상 변경을 Tailwind CSS의 intensity로 표현, 색상 변경 추가

--- fe/src/pages/Calendar.jsx
+++ fe/src/pages/Calendar.jsx
@@ -1,21 +1,34 @@
-import { React, useState, useEffect } from 'react';
+import { React, useState, useEffect, useRef } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import koLocale from '@fullcalendar/core/locales/ko';
 
 import calendarApi from '../api/calendarApi';
 
 export default function Calendar() {
   const navigate = useNavigate();
 
+  const calendarRef = useRef(null);
+
   const [eventList, setEventList] = useState([]);
 
   const [categoryState, setCategoryState] = useState('schedule');
   const [yearState, setYearState] = useState(0);
   const [monthState, setMonthState] = useState(0);
+
+  const [dataColorIndex, setDataColorIndex] = useState(0);
+
+  const colorsArray = [
+    ['bg-green-100', 'bg-green-200', 'bg-green-300', 'bg-green-400', 'bg-green-500'],
+    ['bg-red-100', 'bg-red-200', 'bg-red-300', 'bg-red-400', 'bg-red-500'],
+    ['bg-blue-100', 'bg-blue-200', 'bg-blue-300', 'bg-blue-400', 'bg-blue-500'],
+    ['bg-orange-100', 'bg-orange-200', 'bg-orange-300', 'bg-orange-400', 'bg-orange-500'],
+    ['bg-yellow-100', 'bg-yellow-200', 'bg-yellow-300', 'bg-yellow-400', 'bg-yellow-500'],
+  ];
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -34,7 +47,7 @@ export default function Calendar() {
     { categoryForUrl: 'diary', buttonName: '일기', categoryForApi: 'diaries' },
   ];
 
-  const buttonGroup = buttonAttributiesList.map((buttonAttribute, index) => {
+  const categoryButtonGroup = buttonAttributiesList.map((buttonAttribute, index) => {
     return (
       <div
         key={index}
@@ -48,6 +61,65 @@ export default function Calendar() {
       </div>
     );
   });
+
+  const moveToCurrentMonth = () => {
+    calendarRef.current.getApi().gotoDate(new Date());
+  };
+
+  const moveToPrevMonth = () => {
+    const targetYear = yearState - (monthState === 1);
+    const targetMonth = ((monthState + 10) % 12) + 1;
+    setYearState(targetYear);
+    setMonthState(targetMonth);
+    calendarRef.current
+      .getApi()
+      .gotoDate(`${targetYear}-${targetMonth.toString().padStart(2, '0')}-01`);
+  };
+
+  const moveToNextMonth = () => {
+    const targetYear = yearState + (monthState === 12);
+    const targetMonth = (monthState % 12) + 1;
+    setYearState(targetYear);
+    setMonthState(targetMonth);
+    calendarRef.current
+      .getApi()
+      .gotoDate(`${targetYear}-${targetMonth.toString().padStart(2, '0')}-01`);
+  };
+
+  // TODO: 색상 관련 임시 함수이므로 개발 진척에 따라 삭제 예정
+  const test1 = () => {
+    document.documentElement.style.setProperty('--fc-today-bg-color', '#2799c3');
+  };
+
+  const test2 = () => {
+    document.documentElement.style.setProperty('--fc-today-bg-color', '#27c399');
+  };
+
+  const test3 = () => {
+    setDataColorIndex(() => (dataColorIndex + 1) % colorsArray.length);
+  };
+
+  // TODO: 색상 관련 임시 함수 버튼이 있으므로 개발 진척에 따라 삭제 예정
+  const calendarButtonGroup = [
+    <div key={0} className={`${linkStyle}`} onClick={moveToPrevMonth}>
+      지난 달
+    </div>,
+    <div key={1} className={`${linkStyle}`} onClick={moveToCurrentMonth}>
+      이번 달
+    </div>,
+    <div key={2} className={`${linkStyle}`} onClick={moveToNextMonth}>
+      다음 달
+    </div>,
+    <div key={3} className={`${linkStyle}`} onClick={test1}>
+      색상1
+    </div>,
+    <div key={4} className={`${linkStyle}`} onClick={test2}>
+      색상2
+    </div>,
+    <div key={5} className={`${linkStyle}`} onClick={test3}>
+      색상3
+    </div>,
+  ];
 
   const handleDayCellClick = (e) => {
     navigate(`/day/${e.dateStr}/${categoryState}`);
@@ -74,12 +146,11 @@ export default function Calendar() {
         `${year}-${month.toString().padStart(2, '0')}`,
       );
 
-      const colorArray = ['#AAFFAA', '#55FF55', '#00FF00', '#00BB00', '#007700', '#003300'];
       const addDataArray = response.data.data.map((element) => {
         return {
           date: element.day,
           display: 'background',
-          backgroundColor: colorArray[Math.min(element.count / 3, 5)],
+          count: element.count,
         };
       });
 
@@ -93,20 +164,26 @@ export default function Calendar() {
   // firstDay: 0이면 일요일, 1이면 월요일 시작
   // dayCellClassNames: 각 날짜가 가진 속성('오늘', '과거', '주말' 여부 등)에 따라 className 추가(Tailwind CSS 적용 가능)
   // datesSet: 월 변경 시 함수 실행
-  // titleFormat: 달력 상단에 쓸 제목 설정
   // headerToolbar: 달력 상단에 쓸 제목과 버튼 위치 지정
   return (
     <>
       <section className="pt-4 flex justify-between">
-        <nav className="flex">{buttonGroup}</nav>
+        <nav className="flex">{categoryButtonGroup}</nav>
+        <h2 className="text-3xl">
+          {yearState}년 {monthState}월
+        </h2>
+        <nav className="flex">{calendarButtonGroup}</nav>
       </section>
       <FullCalendar
+        ref={calendarRef}
         plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
         events={eventList}
         dateClick={handleDayCellClick}
         showNonCurrentDates={false}
         firstDay={1}
+        locale={koLocale}
+        eventBackgroundColor="#FFFFFF"
         dayCellClassNames={(arg) => {
           let str = '';
           if (arg.dow === 0) {
@@ -115,18 +192,30 @@ export default function Calendar() {
             str += 'text-blue-500 ';
           }
 
+          const date = `${arg.date.getFullYear()}-${(arg.date.getMonth() + 1).toString().padStart(2, '0')}-${arg.date.getDate().toString().padStart(2, '0')}`;
+
+          for (let i = 0; i < eventList.length; i++) {
+            if (date === eventList[i].date) {
+              str +=
+                colorsArray[dataColorIndex][Math.min(Number.parseInt(eventList[i].count / 3), 4)];
+              break;
+            }
+          }
+
           return str;
         }}
+        dayCellContent={(arg) => ({
+          html: `${arg.date.getDate()}`,
+        })}
         datesSet={(dateInfo) => {
           setYearState(() => dateInfo.start.getFullYear());
           setMonthState(() => dateInfo.start.getMonth() + 1);
           loadList(dateInfo.start.getFullYear(), dateInfo.start.getMonth() + 1, categoryState);
         }}
-        titleFormat={(info) => `${info.date.year}년  ${info.date.month + 1}월`}
         headerToolbar={{
           left: '',
-          center: 'title',
-          right: 'today prev,next',
+          center: '',
+          right: '',
         }}
       />
     </>
